09b0f32b89613040f50f7eb6c4f82ccbb4972883
Alexandra Goldobina <goldobina.s@gmail.com>
2024-09-04 19:00:49 +0500

    модели

---
+++ client/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from mailing.models import NULLABLE
+
+
+class Client(models.Model):
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    patronymic = models.CharField(max_length=50, verbose_name='Отчество', **NULLABLE)
+
+    email = models.EmailField(max_length=100, verbose_name='Почта')
+
+    comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
---
+++ mailing/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class MailingSettings(models.Model):
+    """
+    Настройки рассылки
+    """
+    ONCE_A_DAY = 'OD'
+    ONCE_A_WEEK = 'OW'
+    ONCE_A_MONTH = 'OM'
+    FREQUENCY_CHOICES = [(ONCE_A_DAY, 'Раз в день'), (ONCE_A_WEEK, 'Раз в неделю'), (ONCE_A_MONTH, 'Раз в месяц')]
+
+    start_from = models.DateTimeField(auto_now=False, verbose_name='Дата и время начала рассылки')
+    frequency = models.CharField(max_length=2, choices=FREQUENCY_CHOICES, verbose_name='Периодичность')
+    status = models.CharField(max_length=20, default='created', verbose_name='Статус рассылки')
+    # completed, created, launched
+
+    message = models.ManyToManyField('Message', verbose_name='Сообщение')
+
+    clients = models.ManyToManyField('Client', verbose_name='Клиенты')
+
+    def __str__(self):
+        return f'Дата начала: {self.start_from} Периодичность: {self.frequency} Статус рассылки: {self.status}'
+
+    class Meta:
+        verbose_name = 'Настройка рассылки'
+        verbose_name_plural = 'Настройки рассылок'
+
+
+class Message(models.Model):
+    """
+    Сообщение для рассылки
+    """
+    subject = models.CharField(max_length=250, verbose_name='тема письма')
+    body = models.TextField(verbose_name='тело письма')
+
+    settings = models.ForeignKey(MailingSettings, on_delete=models.CASCADE, verbose_name='настройки')
+
+    def __str__(self):
+        return f'Тема письма: {self.subject}'
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+
+class Log(models.Model):
+    """
+    Лог рассылки
+    """
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата и время попытки')
+    status = models.BooleanField(default=False, verbose_name='статус попытки')
+    response = models.TextField(default='Ответ не получен', verbose_name='ответ почтового сервера')
+
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='сообщение для рассылки')
+
+    def __str__(self):
+        return f' Попытка: {self.pk} Статус попытки отправки: {self.status}'
+
+    class Meta:
+        verbose_name = 'Лог'
+        verbose_name_plural = 'Логи'
