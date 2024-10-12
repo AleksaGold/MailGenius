e3504d7c3bad4ab6246f10e71a79f5325eb078c0
Alexandra Goldobina <goldobina.s@gmail.com>
2024-10-12 10:56:44 +0500

    реализован запуск рассылки из командной строки, фикстуры БД

--- client/cron.py
+++
@@ -1,7 +0,0 @@
-from client.models import Client
-
-
-def add_client():
-    new_client = Client(last_name="Cron", first_name="Crony", email="cron@example.com")
-    new_client.save()
-    print("Client added:", new_client)
